7420c9c663dd706a8e4ddada5aa90c1e4f30eea6
Giampiero Specogna <giampi_12@hotmail.com>
2017-03-19 09:21:42 -0400

    added some comments and organized the general appereance

--- application/views/admin/start.php
+++ application/views/admin/start.php
@@ -16,112 +16,112 @@ if($_POST){
    <p>Usted ha ingresado como <?php echo  $currentUser->email?> <a href="<?php echo base_url('wall/logout') ?>">| Salir |</a></p>
  </div>
  <h2>Muro de Comentarios</h2>
-<div class="jumbotron jb-reduced-in-form">
-  <?php
-    $comment = loadCommentary();
+ <div id='scroll-div'>
+  <div class="jumbotron jb-reduced-in-form">
+    <?php
+      $comment = loadCommentary();
 
-    foreach ($comment as $commentary) {
-      $user = getUserCommentary($commentary->idUser);
-      $response = getRespondonUser($user->id);
-      $photoPath = base_url('').'userPhotos/'.$user->imgContent;
-      echo "<div class='row'>
-              <div class='col-sm-12'>
-                <div class='well well-specific'>
-                <div class='row'>
-                  <div class='col-sm-6'>
-                  <div class = 'row'>
-                    <div class='col-sm-4'>
-                      <img src='{$photoPath}' class='img-circle img-responsive little_img'/>
-                      </div>
-                    <div class='col-sm-6'>
-                      <p class='normal-font'>{$user->name} {$user->lastname}<p>
-                    </div>
-                  </div>
-                  </div>
-                  <div class='col-sm-6'>
-                    <div class='row'>
-                        <div class='col-sm-6'>
-                          <p class='normal-font' id='comment{$commentary->id}'>{$commentary->comment}</p>
-                        ";
-                        ?>
-                          <!-- This link will show me my modal and do all the commentary stuff! -->
-                          <a href="#" class="btn bg-purple" onclick='showModal(<?php echo $commentary->id ?>, <?php echo $user->id ?>,<?php echo $currentUser->id?>)'><i class='fa fa-reply'></i></a>
-                          </div>
-                        <?php
-
-
-
-                    if($user->email == $currentUser->email){
-                      ?>
-                      <!-- In this part i show the delete and edit properties of the comments -->
-                      <div class="col-sm-6">
-                        <a href="#" class=' btn bg-purple' onclick="confirmationDelete('<?php echo $commentary->id ?>');"><i class='fa fa-trash'></i></a>
-                        <a href="#" class='btn bg-purple' onclick="confirmationEdit('<?php echo $commentary->id ?>','<?php echo $commentary->comment ?>');"><i class="fa fa-pencil-square-o"></i></a>
-                      </div>
-                    </div>
-
-                      <?php
-                    }
-                    //This is my modal  that will appear when i click on the link
-                    echo"<div id='responseModal{$user->id}' class='modal fade' role='dialog'>
-                          <div class='modal-dialog'>
-                            <div class='modal-content'>
-                              <div class='modal-header'>
-                                <button type='button'class='close' data-dismiss='modal'>&times;</button>
-                                <h4 class='modal-title'>Responda a este comentario</h4>
-                              </div>
-                              <div class='modal-body'>
-                                  <div class = 'row'>
-                                    <div class='col-sm-6'>
-                                      <textarea name='response-text' rows='1' id='response-text{$user->id}' cols='80' class='form-control' placeholder='responda'></textarea>
-                                    </div>
-                                  </div>
-                              </div>
-                              <div class='modal-footer'>
-                              <button type='button' class='btn btn-default' id='btn-respond{$user->id}'>Responder</button>
-                            </div>
-                            </div>
-
-                          </div>
-                        </div>";
-
-                echo "</div>
-                </div>
-                </div>
-              </div>
-                </div>
-            </div>";
-//In this code i go and search for all my responses
-            foreach ($response as $rs) {
-              $userRespond = getUserCommentary($rs->currentUser);
-              $photoPath = base_url('').'userPhotos/'.$userRespond->imgContent;
-              if($commentary->id== $rs->idCommentary){
-                echo"
-                <div class='panel panel-default marginated-to-right'>
-                  <div class='panel body'>
+      foreach ($comment as $commentary) {
+        $user = getUserCommentary($commentary->idUser);
+        $response = getRespondonUser($user->id);
+        $photoPath = base_url('').'userPhotos/'.$user->imgContent;
+        echo "<div class='row'>
+                <div class='col-sm-12'>
+                  <div class='well well-specific'>
                   <div class='row'>
-                        <div class='col-sm-6'>
-                            <div class='row'>
-                              <div class='col-sm-4'>
-                              <img src='{$photoPath}' class='img-circle img-responsive little_img'/>
-                              </div>
-                              <div class='col-sm-6'>
-                                <p>{$userRespond->name} {$userRespond->lastname} </p>
-                              </div>
-                            </div>
-                          </div>
-                        <div class='col-sm-6'>
-                          <p> $rs->comment</p>
+                    <div class='col-sm-6'>
+                    <div class = 'row'>
+                      <div class='col-sm-4'>
+                        <img src='{$photoPath}' class='img-circle img-responsive little_img'/>
                         </div>
+                      <div class='col-sm-6'>
+                        <p class='normal-font'>{$user->name} {$user->lastname}<p>
+                      </div>
+                    </div>
+                    </div>
+                    <div class='col-sm-6'>
+                      <div class='row'>
+                          <div class='col-sm-6'>
+                            <p class='normal-font' id='comment{$commentary->id}'>{$commentary->comment}</p>";
+                          ?>
+                            <!-- This link will show me my modal and do all the commentary stuff! -->
+                            <a href="#" class="btn bg-purple" onclick='showModal(<?php echo $commentary->id ?>, <?php echo $user->id ?>,<?php echo $currentUser->id?>)'><i class='fa fa-reply'></i></a>
+                            </div>
+                          <?php
+
+
+
+                      if($user->email == $currentUser->email){
+                        ?>
+                        <!-- In this part i show the delete and edit properties of the comments -->
+                        <div class="col-sm-6">
+                          <a href="#" class=' btn bg-purple' onclick="confirmationDelete('<?php echo $commentary->id ?>');"><i class='fa fa-trash'></i></a>
+                          <a href="#" class='btn bg-purple' onclick="confirmationEdit('<?php echo $commentary->id ?>','<?php echo $commentary->comment ?>');"><i class="fa fa-pencil-square-o"></i></a>
+                      </div>
+                        <?php
+                      }
+                      /*echo"</div>
+                      </div>";*/
+                      //This is my modal  that will appear when i click on the link
+                      echo"<div id='responseModal{$user->id}' class='modal fade' role='dialog'>
+                            <div class='modal-dialog'>
+                              <div class='modal-content'>
+                                <div class='modal-header'>
+                                  <button type='button'class='close' data-dismiss='modal'>&times;</button>
+                                  <h4 class='modal-title'>Responda a este comentario</h4>
+                                </div>
+                                <div class='modal-body'>
+                                    <div class = 'row'>
+                                      <div class='col-sm-6'>
+                                        <textarea name='response-text' rows='1' id='response-text{$user->id}' cols='80' class='form-control' placeholder='responda'></textarea>
+                                      </div>
+                                    </div>
+                                </div>
+                                <div class='modal-footer'>
+                                <button type='button' class='btn btn-default' id='btn-respond{$user->id}'>Responder</button>
+                              </div>
+                              </div>
+
+                            </div>
+                          </div>";
+
+                  echo "</div>
                   </div>
                   </div>
                 </div>
-                ";
+                  </div>
+                </div>";
+  //In this code i go and search for all my responses
+              foreach ($response as $rs) {
+                $userRespond = getUserCommentary($rs->currentUser);
+                $photoPath = base_url('').'userPhotos/'.$userRespond->imgContent;
+                if($commentary->id== $rs->idCommentary){
+                  echo"
+                  <div class='panel panel-default marginated-to-right'>
+                    <div class='panel body'>
+                    <div class='row'>
+                          <div class='col-sm-6'>
+                              <div class='row'>
+                                <div class='col-sm-4'>
+                                <img src='{$photoPath}' class='img-circle img-responsive little_img'/>
+                                </div>
+                                <div class='col-sm-6'>
+                                  <p>{$userRespond->name} {$userRespond->lastname}</p>
+                                </div>
+                              </div>
+                            </div>
+                          <div class='col-sm-6'>
+                            <p>$rs->comment</p>
+                          </div>
+                    </div>
+                    </div>
+                  </div>";
+                }
               }
-            }
-    }
+      }
 
-   ?>
+     ?>
+  </div>
 </div>
 <!-- Here i comment normally -->
 <div class="jumbotron jb-reduced">
@@ -188,9 +188,5 @@ if($_POST){
     }
 
   }
-  function insertResponse(){
 
-
-
-  }
 </script>
