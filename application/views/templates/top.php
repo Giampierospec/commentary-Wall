8b97d210d7d1e8ebc8a28add59b2fb61c89a2a4a
Giampiero Specogna <giampi_12@hotmail.com>
2017-03-24 09:57:03 -0400

    Hosted everyhing locally

--- application/views/templates/top.php
+++ application/views/templates/top.php
@@ -4,21 +4,20 @@
   <head>
     <meta charset="utf-8">
     <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link rel="stylesheet" href=<?php echo base_url('') ?>"bootstrap/css/bootstrap.min.css">
 
 <!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<link rel="stylesheet" href=<?php echo base_url('') ?>"bootstrap/css/bootstrap-theme.min.css">
+<script src="js/jquery.min.js"></script>
 
 <!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-<script src="https://use.fontawesome.com/01d9e54a7d.js"></script>
+<script src=<?php echo base_url('') ?>"bootstrap/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href=<?php  echo base_url('')?>"/css/animate.css">
 <link rel="stylesheet" href="<?php echo base_url('') ?>css/login.css">
 <link rel="stylesheet" href="<?php echo base_url('') ?>css/main.css" />
 <link rel="stylesheet" href="<?php echo base_url('') ?>css/register.css">
 <link rel="stylesheet" href="<?php echo base_url('') ?>css/commentWall.css">
 <link href="https://fonts.googleapis.com/css?family=Lobster|Ranga" rel="stylesheet">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
     <title><?php echo $data['title'] = "Muro Mensajes  | Tarea 9"?></title>
   </head>
   <body>
