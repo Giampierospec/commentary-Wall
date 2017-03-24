7593b9c583de65f5dcdf6e79fe419f5ba24a5515
Giampiero Specogna <giampi_12@hotmail.com>
2017-03-24 10:19:56 -0400

    Hosted font-awesome locally

--- application/views/templates/top.php
+++ application/views/templates/top.php
@@ -4,19 +4,20 @@
   <head>
     <meta charset="utf-8">
     <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href=<?php echo base_url('') ?>"bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="<?php echo base_url('') ?>bootstrap/css/bootstrap.min.css">
 
 <!-- Optional theme -->
-<link rel="stylesheet" href=<?php echo base_url('') ?>"bootstrap/css/bootstrap-theme.min.css">
-<script src="js/jquery.min.js"></script>
+<link rel="stylesheet" href="<?php echo base_url('') ?>bootstrap/css/bootstrap-theme.min.css">
+<script src="<?php echo base_url('') ?>js/jquery.min.js"></script>
 
 <!-- Latest compiled and minified JavaScript -->
-<script src=<?php echo base_url('') ?>"bootstrap/js/bootstrap.min.js"></script>
-<link rel="stylesheet" href=<?php  echo base_url('')?>"/css/animate.css">
-<link rel="stylesheet" href="<?php echo base_url('') ?>css/login.css">
-<link rel="stylesheet" href="<?php echo base_url('') ?>css/main.css" />
-<link rel="stylesheet" href="<?php echo base_url('') ?>css/register.css">
-<link rel="stylesheet" href="<?php echo base_url('') ?>css/commentWall.css">
+<script src="<?php echo base_url('')?>bootstrap/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="<?php  echo base_url('')?>css/animate.css">
+<link rel="stylesheet" href="<?php echo base_url('')?>css/login.css">
+<link rel="stylesheet" href="<?php echo base_url('')?>css/main.css" />
+<link rel="stylesheet" href="<?php echo base_url('')?>css/register.css">
+<link rel="stylesheet" href="<?php echo base_url('')?>css/commentWall.css">
+<link rel="stylesheet" href="<?php echo base_url('') ?>font-awesome/css/font-awesome.css">
 <link href="https://fonts.googleapis.com/css?family=Lobster|Ranga" rel="stylesheet">
     <title><?php echo $data['title'] = "Muro Mensajes  | Tarea 9"?></title>
   </head>
